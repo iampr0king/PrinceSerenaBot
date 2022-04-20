3e7ba7c39593c7f9148c148e97d385ea6a02a91f
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-04-20 06:54:26 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -268,7 +268,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./MashaRoBot/resources/*")
+    fnt = glob.glob("./MashaRoBot/resources/LOGOS/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)

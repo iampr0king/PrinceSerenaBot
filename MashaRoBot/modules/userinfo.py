c548c8fcd0891a218a816648fc92381629f85612
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-05-11 17:05:48 +0530

    Update userinfo.py

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="tg://settings">?</a>]'.format(
+        text += ' [<a href="https://t.me/call_me_cRaZyBoY">?</a>]'.format(
             bot.username
         )
 

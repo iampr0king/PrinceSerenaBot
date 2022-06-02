7ba7b4b9a2ceaf8fd6099b294ee9629466c4086b
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-06-02 12:22:07 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -79,7 +79,7 @@ buttons = [
     [
         InlineKeyboardButton(text="👨‍💻 𝐎𝐖𝐍𝐄𝐑", url="https://t.me/itzmecrazy"),
         InlineKeyboardButton(
-            text="🤗 CHAT GROUP", url="https://t.me/InternationalTamilChatting"
+            text="🤗 CHAT GROUP", url="https://t.me/TamilChat_CRINGE_GUYS"
         ),
     ],
     [
